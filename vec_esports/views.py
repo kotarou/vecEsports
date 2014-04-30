acdbf231ed780a3325026458ac77a610a2cf8f16
Kieran Carnegie <kotarou@ecs.vuw.ac.nz>
2014-04-30 15:34:51 +1200

    Team creation now fails on duplicate team creation

--- vec_esports/views.py
+++ vec_esports/views.py
@@ -61,35 +61,39 @@ def team_register(request):
     if request.method == 'POST':
         team_name = request.POST.get('tmn')
 
-        # q = db.GqlQuery('SELECT * FROM Team WHERE name = :1', team_name)
-        # team_one = q.fetch(1)[0]
-        # print(team_one)
-
-        team_captain = request.POST.get('cap')
-        team_p2 =  request.POST.get('pl2')
-        team_p3 =  request.POST.get('pl3')
-        team_p4 =  request.POST.get('pl4')
-        team_p5 =  request.POST.get('pl5')
-        team_p6 =  request.POST.get('pl6')
-        team_p7 =  request.POST.get('pl7')
-        team_contact =  request.POST.get('eml')
-        team = Team(key_name=team_name,
-                    name=team_name,
-                    captain=team_captain,
-                    player_2=team_p2,
-                    player_3=team_p3,
-                    player_4=team_p4,
-                    player_5=team_p5,
-                    player_6=team_p6,
-                    player_7=team_p7,
-                    contact_email=team_contact)
-        team.put()
-
-        team_vars.update({
-            'last_operation': "Team Registration",
-            'lo_value': "Success",
-            'lo_reason': ""
-        })
+        q = db.GqlQuery('SELECT * FROM Team WHERE name = :1', team_name)
+        team_prexist = q.fetch(1)
+        if(len(team_prexist) > 0):
+            team_vars.update({
+                'last_operation': "Team Registration",
+                'lo_value': "Fail",
+                'lo_reason': "Team already exists"
+            })
+        else:
+            team_captain = request.POST.get('cap')
+            team_p2 =  request.POST.get('pl2')
+            team_p3 =  request.POST.get('pl3')
+            team_p4 =  request.POST.get('pl4')
+            team_p5 =  request.POST.get('pl5')
+            team_p6 =  request.POST.get('pl6')
+            team_p7 =  request.POST.get('pl7')
+            team_contact =  request.POST.get('eml')
+            team = Team(key_name=team_name,
+                        name=team_name,
+                        captain=team_captain,
+                        player_2=team_p2,
+                        player_3=team_p3,
+                        player_4=team_p4,
+                        player_5=team_p5,
+                        player_6=team_p6,
+                        player_7=team_p7,
+                        contact_email=team_contact)
+            team.put()
+            team_vars.update({
+                'last_operation': "Team Registration",
+                'lo_value': "Success",
+                'lo_reason': ""
+            })
     return direct_to_template(request, 'esports/team.html', team_vars)
 
 
